94ba7920c1fe9582784a1054fcc76b5d4432c691
Manish Kumar <rmanish7836@gmail.com>
2024-05-31 00:07:05 +0530

    Json Response for individual elements

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('list/',views.movie_list,name='movie_list'),
+    path('<int:pk>',views.movie_details,name='movie_details'),
 ]
--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -7,8 +7,22 @@ from django.http import JsonResponse
 def movie_list(request):
     movies = models.Movie.objects.all()
     #print(list(movies.values()))
+    print(movies.name)
     data = {
         'movies': list(movies.values())
         }
 
+    return JsonResponse(data)
+
+def movie_details(request, pk):
+    movie = models.Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'description':movie.description,
+        'active': movie.active
+    }
+    # Here we cannot use .values() method becasue .get() return single modle instace.
+    # And for single model instace .value() is not available.
+    # .value() is only available for QuerySet Object.
+    # so we can only access data using attributes like movie.name or movie.status etc
     return JsonResponse(data)
