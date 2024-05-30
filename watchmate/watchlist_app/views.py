28988234c7fc92e508c57c05587f978d89be7e49
Manish Kumar <rmanish7836@gmail.com>
2024-05-30 22:17:55 +0530

    Sending JSON Response

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . import models
+from django.http import JsonResponse
 
 # Create your views here.
+
+def movie_list(request):
+    movies = models.Movie.objects.all()
+    #print(list(movies.values()))
+    data = {
+        'movies': list(movies.values())
+        }
+
+    return JsonResponse(data)
