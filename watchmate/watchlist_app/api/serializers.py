faaa14bc39cd7fdb289bc97ef15b7d1a95c07a34
Manish Kumar <rmanish7836@gmail.com>
2024-06-01 22:24:59 +0530

    added POST, PUT & DELETE method

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
+from watchlist_app import models
 
 class MovieSerializer(serializers.Serializer): #convention of naming class ModelSerializer
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self, validated_data):
+        return models.Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        # instance -> old values
+        #validated_data -> new values
+
+        #below is auto generated without any field.
+        #return super().update(instance, validated_data)
+
+        #according to insturctr we need to map each field.
+        instance.name = validated_data.get('name', instance.name) #update name field
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+    
+    
+    
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -3,31 +3,60 @@ from rest_framework.decorators import api_view
 from watchlist_app import models
 from . import serializers
 
-@api_view() #by default api_view() decorator is set to get. we can use @api_view(['GET','POST']) for extra methods
+@api_view(['GET','POST']) #by default api_view() decorator is set to get. we can use @api_view(['GET','POST']) for extra methods
 def movie_list(request):
-    movies = models.Movie.objects.all()
-    serializer = serializers.MovieSerializer(movies, many=True)
-    """
-    variable name can be anything it is not mandatory to use serializer as variable name.
-    serializers work on only single object of QuerySet but here we are passing .all()
-    in movies vairable which is fetching mutliple objects within a QuerySet.
-    That's the reason we need to set the serializer many=True, so that it'll know it might get
-    mutliptle objects so it need to traverse to each object.
-    """
-    return Response(serializer.data)
-    """
-    now serializer has converted the data  but it's still a object and to access that data
-    we need to user attribute like we used previously movies.name. movies.active etc
-    but instead of accessing one by one we can just now say serializer.data it'll get all fields data for us. 
-    """
+    if request.method =='GET':
+        movies = models.Movie.objects.all()
+        serializer = serializers.MovieSerializer(movies, many=True)
+        """
+        variable name can be anything it is not mandatory to use serializer as variable name.
+        serializers work on only single object of QuerySet but here we are passing .all()
+        in movies vairable which is fetching mutliple objects within a QuerySet.
+        That's the reason we need to set the serializer many=True, so that it'll know it might get
+        mutliptle objects so it need to traverse to each object.
+        """
+        return Response(serializer.data)
+        """
+        now serializer has converted the data  but it's still a object and to access that data
+        we need to user attribute like we used previously movies.name. movies.active etc
+        but instead of accessing one by one we can just now say serializer.data it'll get all fields data for us. 
+        """ 
+    
+    if request.method == 'POST':
+        serializer = serializers.MovieSerializer(data=request.data)
+        #created a serializer object from recieved data
 
-@api_view()
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+@api_view(['GET','PUT','DELETE'])
 def movie_details(request, pk):
-    movie = models.Movie.objects.get(pk=pk)
-    serializer = serializers.MovieSerializer(movie)
-    return Response(serializer.data)
-    """
-    now serializer has converted the data  but it's still a object and to access that data
-    we need to user attribute like we used previously movies.name. movies.active etc
-    but instead of accessing one by one we can just now say serializer.data it'll get all fields data for us. 
-    """
+    if request.method == 'GET':
+        movie = models.Movie.objects.get(pk=pk)
+        serializer = serializers.MovieSerializer(movie)
+        return Response(serializer.data)
+        """
+        now serializer has converted the data  but it's still a object and to access that data
+        we need to user attribute like we used previously movies.name. movies.active etc
+        but instead of accessing one by one we can just now say serializer.data it'll get all fields data for us. 
+        """
+    
+    if request.method == 'PUT':
+        movie = models.Movie.objects.get(pk=pk) 
+        serializer = serializers.MovieSerializer(movie, data=request.data) 
+        #created a serializer object from recieved data for the object which we have passed.
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)        
+
+    if request.method == 'DELETE':
+        movie = models.Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response('you data is deleted.')
+
